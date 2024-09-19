0e62d5ae89e88945c8dcc340c015b289079b1645
Joao Bertholo <joao.bertholo@gmail.com>
2024-09-19 13:38:09 +0100

    added html page

--- js/index.js
+++ js/index.js
@@ -1,6 +1,16 @@
-const navToggle = document.querySelector('.nav-toggle');
+const data = [
+    { aLink: "bertholo.github.io/sales-rep-ana/", imgSrc: "img/direcionaco.jpg" },
+    { aLink: "budget-buddy.html", imgSrc: "img/budget-buddy-front-page.jpg" },
+    { aLink: "bertholo.github.io/weather-app/", imgSrc: "img/weather-app-front-page.jpg" },
+    { aLink: "ravenous.html", imgSrc: "img/background_search_mobile.jpg" },
+    { aLink: "jamming.html", imgSrc: "img/background_photo_jamming.jpg" },
+    { aLink: "boss-machine.html", imgSrc: "img/boss-machine-meetings.png" }
+];
 
+const navToggle = document.querySelector('.nav-toggle');
 const navLinks = document.querySelectorAll('.nav__link');
+const showInfo = document.querySelectorAll('.show-info');
+const portfolio = document.querySelector('.portfolio');
 
 navToggle.addEventListener('click', () => {
     document.body.classList.toggle('nav-open');
@@ -11,3 +21,28 @@ navLinks.forEach(link => {
         document.body.classList.remove('nav-open');
     })
 })
+
+window.addEventListener('DOMContentLoaded', () => {
+    data.forEach(item => {
+        const portfolioItem = document.createElement('div');
+
+        portfolioItem.classList.add('portfolio-item');
+
+        portfolioItem.style.backgroundImage = `url(${item.imgSrc})`;
+        portfolioItem.style.backgroundSize = 'cover';
+        portfolioItem.style.backgroundPosition = 'center';
+        portfolioItem.style.cursor = 'pointer';
+
+        portfolioItem.addEventListener('click', () => {
+            window.location.href = item.aLink;
+        });
+
+        portfolio.appendChild(portfolioItem);
+
+        portfolioItem.addEventListener('mouseover', () => {
+            
+        })
+    })
+})
+
+
